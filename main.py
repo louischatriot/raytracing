fb9cc13512ca79c146974eb73947b3c2737f7b3c
Louis Chatriot <louis.chatriot@gmail.com>
2019-05-27 22:49:44 +0200

    Added intersect sphere

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from math import cos, sin, pi
+from math import cos, sin, pi, sqrt
 
 
 class Vector:
@@ -6,6 +6,9 @@ class Vector:
         self.x = x
         self.y = y
         self.z = z
+
+    def norm(self):
+        return sqrt(self.x * self.x + self.y * self.y + self.z * self.z)
 
     def __str__(self):
         return f"<Vector> {self.x} ; {self.y} ; {self.z}"
@@ -16,24 +19,55 @@ class Vector:
     def __sub__(self, other):
         return Vector(self.x - other.x, self.y - other.y, self.z - other.z)
 
+    # vector times scalar
+    def __mul__(self, other):
+        return Vector(self.x * other, self.y * other, self.z * other)
 
-v = Vector(1, 4, 77)
-w = Vector(1000, 2000, 3000)
-
-print(v)
-print(w)
-print(w - v)
-
-print(cos(pi / 4))
 
 # a = alpha = angle on (x, y)
 # t = theta = angle on ((x,y), z)
 def calc_vectors(a, t):
     v = Vector(cos(a) * cos(t), sin(a) * cos(t), sin(t))
-    w = Vactor(-v.y / cos(t), v.x / cos(t), 0)
+    w = Vector(-v.y / cos(t), v.x / cos(t), 0)
     h = Vector(-sin(t) * cos(a), -sin(t) * sin(a), cos(t))
 
     return v, w, h
+
+
+# Returns closest intersection point from eye direction to sphere if any
+# e_p (vector) = point E, the eye
+# m_p (vector) = point M, the pixel on the screen
+# c_p (vector) = point C = center of the sphere
+# r = sphere radius
+def intersect_sphere(e_p, m_p, c_p, r):
+    m = m_p - e_p
+    ec = c_p - e_p
+
+    a = m.x * m.x + m.y * m.y + m.z * m.z
+    b = 2 * (m.x * ec.x + m.y * ec.y + m.z * ec.z)
+    c = ec.x * ec.x + ec.y * ec.y + ec.z * ec.z
+
+    d = b * b - 4 * a * c
+    if d < 0:
+        return None
+
+    if d == 0:
+        t = -b / (2 * a)
+        return e + m * t
+
+    t1 = (-b + sqrt(d)) / (2 * a)
+    t2 = (-b - sqrt(d)) / (2 * a)
+
+    i1 = e + m * t1
+    i2 = e + m * t2
+
+    if (i1 - e_p).norm() > (i2 - e_p).norm():
+        return i2
+    else:
+        return i1
+
+
+
 
 
 
